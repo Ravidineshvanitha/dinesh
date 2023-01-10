a353c7ed086106e6c170f66c59ec82c25f1301e9
Ravidineshvanitha <122258152+Ravidineshvanitha@users.noreply.github.com>
2023-01-10 10:28:05 +0530

    Add files via upload

---
+++ day 2 pro 3.py
@@ -0,0 +1,7 @@
+count0fWords=len("Hello This is python Programming".split())
+print("Count of Words in the given Sentence:",count0fWords)
+
+print(len("Hello This is python Programming".split()))
+
+print(len(input("Enter Input:").split()))
+      
