b83d4f4e077df6d104325ade2db6f1021f596859
Ravidineshvanitha <122258152+Ravidineshvanitha@users.noreply.github.com>
2023-01-19 09:02:43 +0530

    Add files via upload

---
+++ day5 pro2.py
@@ -0,0 +1,9 @@
+Enter the grade of the employee: A
+Enter the employee salary: 8000
+Enter the grade of the employee: C
+Enter the employee salary: 60000
+Enter the grade of the employee: B
+Enter the employee salary: 0
+Enter the grade of the employee: 38000
+Enter the employee salary: A
+Enter the grade of the employee: B
